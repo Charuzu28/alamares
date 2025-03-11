9629da2ae6d3eb7f44eac16e1fc34c54d0f2dabb
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-03-11 17:13:27 +0800

    added delete function

--- src/app/posts/post.service.ts
+++ src/app/posts/post.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({providedIn: 'root'})
 export class PostsService{
@@ -14,10 +15,19 @@ export class PostsService{
 
     getPosts(){
         this.http.get<{message: string, posts: Post[]}>("http://localhost:3000/api/posts")
-        .subscribe((postData) => {
-           this.posts = postData.posts;
-           this.postsUpdated.next([...this.posts]);
-        });
+        .pipe(map((postData) => {
+            return postData.posts.map((post:any) => {
+                return {
+                    title: post.title,
+                    content: post.content,
+                    id: post._id
+                }
+            })
+        }))
+        .subscribe((transformedPosts) => {
+            this.posts = transformedPosts;
+            this.postsUpdated.next([...this.posts]);
+        })
     }
     getPostUpdatedListener(){
         return this.postsUpdated.asObservable();
@@ -32,5 +42,14 @@ export class PostsService{
             this.posts.push(post);
             this.postsUpdated.next([...this.posts]);
           });
+    }
+    deletePost(postId: string){
+        this.http.delete("http://localhost:3000/api/posts/" + postId)
+        .subscribe(() => {
+            console.log('Deleted');
+            const updatedPosts = this.posts.filter(post => post.id !== postId);
+            this.posts = updatedPosts;
+            this.postsUpdated.next([...this.posts]);
+        })
     }
 }
