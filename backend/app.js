7c3a2600690f0556f16c37229ed3a715fafeeaf5
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-03-21 17:44:53 +0800

    Added router & spinner

--- backend/app.js
+++ backend/app.js
@@ -1,73 +1,81 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const Post = require('./models/post');
-const app = express();
 const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const postroutes = require('./routes/posts');  
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//home IP
-//o8V3JsROqvy8Gil5
-//DWCL
-//uTAlCpvxZWvGXoEt
-  //connect
-  // mongoose.connect('mongodb+srv://07212533:o8V3JsROqvy8Gil5@cluster0.2w7ko.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  mongoose.connect('mongodb+srv://charls:uTAlCpvxZWvGXoEt@cluster0.uflwr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
-  .then(()=>{
-    console.log('Connected to database!')
-  }).catch(()=>{
-    console.log("Error connecting to Database")
-  })
+app.use(cors()); 
+
+mongoose.connect('mongodb+srv://charls:uTAlCpvxZWvGXoEt@cluster0.uflwr.mongodb.net/blog?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Connected to database!');
+  }).catch(() => {
+    console.log("Error connecting to Database");
+  });
 
 app.get("/", (req, res) => {
-    res.send("This is the homepage");
-  });
-  
-  app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*"),
-      res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-  
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PATCH, DELETE, OPTIONS"
-    );
-  
-    next();
-  });
-
-
-  
-  app.post("/api/posts", (req, res, next) => {
-    const post = new Post({
-      title: req.body.title,
-      content: req.body.content
-    });
-    post.save();
-    res.status(201).json({
-      message: "Post added successfully",
-    });
-  });
-
-app.get("/api/posts",(req, res, next) => {
-    Post.find()
-        .then(documents => {
-          res.status(200).json({
-            message: 'Post successfully fetched',
-            posts: documents
-          });
-        });
+  res.send("This is the homepage");
 });
 
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  });
+  post.save().then(result => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: result._id
+    });
+  });
+});
+
+app.get("/api/posts", (req, res, next) => {
+  Post.find()
+    .then(documents => {
+      res.status(200).json({
+        message: 'Posts successfully fetched',
+        posts: documents
+      });
+    });
+});
+
+app.get("/api/posts/:id", (req, res, next) => {
+  Post.findById(req.params.id).then(post => {
+    if (post) {
+      res.status(200).json(post);
+    } else {
+      res.status(404).json({ message: "Post not found!" });
+    }
+  });
+}); 
+
+
+app.put("/api/posts/:id", (req, res, next) => {
+  const post = new Post({
+    _id: req.body.id,
+    title: req.body.title,
+    content: req.body.content
+  });
+  Post.updateOne({ _id: req.params.id }, post).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Update Successful!" });
+  });
+});
+
+
 app.delete("/api/posts/:id", (req, res, next) => {
-    Post.deleteOne({_id: req.params.id }).then(result => {
-      console.log(result);
-      console.log(req.param.id);
-      res.status(200).json({message: "Post Deleted"});
-    })
-})
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log(result);
+    res.status(200).json({ message: "Post Deleted" });
+  });
+});
+
+app.use(postroutes);  
 
 module.exports = app;
