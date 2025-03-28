eb8bf22836caf0a90dbcaef40ed32dcbf09a0349
Charuzu28 <146437969+Charuzu28@users.noreply.github.com>
2025-03-28 10:18:57 +0800

    added delete function

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -104,4 +104,11 @@ router.get("/", (req, res) => {
   });
 });
 
+router.delete("/:id", (req, res) => {
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    console.log("Deleted:", result);
+    res.status(200).json({ message: "Post deleted" });
+  });
+});
+
 module.exports = router;
